71bf0418c169ccf2f247a561354a8b9a43a871ba
NiniLovely1 <ninitaylor78@gmail.com>
2024-11-27 10:33:55 -0700

    Updated search function using Api

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
-function refreshWeatherData(response) {
-  console.log(response.data);
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityWeatherElement = document.querySelector("#city-weather");
+
+  cityWeatherElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "46a7o7054af22ba1t49c85122b081838";
-  let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
-  axios.get(apiUrl).then(refreshWeatherData);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityWeatherElement = document.querySelector("#city-weather");
-  cityWeatherElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Gianyar");
